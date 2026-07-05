efdceb326f7607cf484cac328ea1b2c3afa0eb50
Usama <usama.aslam@gmail.com>
2026-07-05 11:08:18 +0100

    resolve properties setting issues

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Property(models.Model):
+    PROPERTY_TYPE = (
+        ('house', 'House'),
+        ('flat', 'Flat'),
+        ('apartment', 'Apartment'),
+        ('studio', 'Studio'),
+    )
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={'role': 'landlord'}
+    )
+
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    property_type = models.CharField(max_length=20, choices=PROPERTY_TYPE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
